43031bc6348d091a003000fd87909407a8845055
AMAN PATEL <ap289503@gmail.com>
2022-04-28 13:01:31 +0530

    updated solution of LC#1631

--- LC#1631_Path_With_Minimum_Effort.java
+++ LC#1631_Path_With_Minimum_Effort.java
@@ -8,11 +8,12 @@ class Solution {
         int[][] min_effort_grid = new int[m][n];
         for (int[] r : min_effort_grid) Arrays.fill(r, iMax);
         min_effort_grid[0][0] = 0;
-        Queue<int[][]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[1][0]));
-        pq.offer(new int[][]{{0, 0}, {0}});
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[2]));
+        pq.offer(new int[]{0, 0, 0});
         while (!pq.isEmpty()) {
-            int[][] top = pq.poll();
-            int r = top[0][0], c = top[0][1], e = top[1][0];
+            int[] top = pq.poll();
+            int r = top[0], c = top[1], e = top[2];
+            if (r == (m - 1) && c == (n - 1)) return min_effort_grid[r][c];
             if (min_effort_grid[r][c] < e) continue;
             for (int i = 0; i < 4; i++) {
                 int r_new = r + dir[i][0], c_new = c + dir[i][1];
@@ -21,11 +22,11 @@ class Solution {
                                      Math.abs(heights[r][c] - heights[r_new][c_new]));
                 if (min_effort_grid[r_new][c_new] > e_new) {
                     min_effort_grid[r_new][c_new] = e_new;
-                    pq.offer(new int[][]{{r_new, c_new}, {e_new}});
+                    pq.offer(new int[]{r_new, c_new, e_new});
                 }
             }
         }
         
-        return min_effort_grid[m - 1][n - 1];
+        return -1;
     }
 }
